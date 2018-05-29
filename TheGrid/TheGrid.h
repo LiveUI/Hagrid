0ddd2dbada7b16a17d12ad3f8c568444a6bef875
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-05-29 13:09:50 +0100

    Initial Commit

---
+++ TheGrid/TheGrid.h
@@ -0,0 +1,19 @@
+//
+//  TheGrid.h
+//  TheGrid
+//
+//  Created by Ondrej Rafaj on 29/05/2018.
+//  Copyright © 2018 LiveUI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TheGrid.
+FOUNDATION_EXPORT double TheGridVersionNumber;
+
+//! Project version string for TheGrid.
+FOUNDATION_EXPORT const unsigned char TheGridVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TheGrid/PublicHeader.h>
+
+
